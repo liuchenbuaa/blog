adff827bd6f1eb859a154c01cc6b08ad37c5dbf4
liuchen <liuchen@zrcaifu.com>
2016-07-15 16:32:56 +0800

    dev

--- src/index.js
+++ src/index.js
@@ -1,48 +1,13 @@
 var koa = require('koa');
 var views = require('koa-views');
-var mongo = require('./mongo');
 var app = new koa();
 
 app.use(views(__dirname + '/views',{
     map: {html: 'nunjucks'}
 }));
 
-app.use(mongo.default);
+app.use(require('./mongo').default);
 
-/*
-var MongoClient = require('mongodb').MongoClient;
-var assert = require('assert');
-
-
-// Connection URL
-var url = 'mongodb://127.0.0.1/person';
-// Use connect method to connect to the Server
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected correctly to server");
-  insertDocuments(db, function() {
-    db.close();
-  });
-});
-
-var insertDocuments = function(db, callback) {
-  // Get the documents collection
-  var collection = db.collection('documents');
-  // Insert some documents
-  collection.insertMany([
-    {a : 1}, {a : 2}, {a : 3}
-  ], function(err, result) {
-    assert.equal(err, null);
-    assert.equal(3, result.result.n);
-    assert.equal(3, result.ops.length);
-    console.log("Inserted 3 documents into the document collection");
-    callback(result);
-  });
-}
-*/
-
-//app.use(require('koa-bodyparser')());
 require('./routes').default(app);
-
 
 app.listen(3000);
--- src/middleware/mongo.js
+++ src/middleware/mongo.js
@@ -1,8 +1,10 @@
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 
+console.log("11111");
 var db;
 var url = 'mongodb://127.0.0.1/person';
+console.log("222222");
 
 MongoClient.connect(url, function(err, db) {
   assert.equal(null, err);
@@ -11,9 +13,14 @@ MongoClient.connect(url, function(err, db) {
   /*insertDocuments(db, function() {
     db.close();
   });*/
+  console.log("333333");
 });
 
+console.log("4444444");
+
 export default async function(ctx, next){
+  console.log("5555555");
   ctx.mongo=db;
   await next();
+  console.log("66666666");
 }
