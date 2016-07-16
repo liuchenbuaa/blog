73b14cde37a9b8176ca5d1a4a010cb87e5eb07d3
liuchen <liuchen@zrcaifu.com>
2016-07-17 01:17:27 +0800

    dev

--- src/routes/message.js
+++ src/routes/message.js
@@ -2,13 +2,53 @@ var router = require('koa-router')();
 var assert = require('assert');
 var markdown = require('markdown').markdown;
 
-router.get('/list', async (ctx,next)=>{
-    ctx.body="hello world";
+router.post('/list', async (ctx,next)=>{
+    var {page = 1,pagesize = 10} = ctx.request.body;
+    var offset = (page - 1) * pagesize;
+    if(page && pagesize){
+        var collection = ctx.mongo.collection('messages');
+        collection.find([
+          {}
+        ], {limit:pagesize,skip:offset} ,function(err, result) {
+            assert.equal(err, null);
+            console.log(result);
+        });
+        ctx.body = {success:true};
+    }else{
+        ctx.body = {success:false};
+    }
+});
+
+router.post('/delete', async (ctx, next)=>{
+    var {content,name} = ctx.request.body;
+    if(content && name){
+        var collection = ctx.mongo.collection('messages');
+        // Insert some documents
+        collection.insertMany([
+          {content:content,name:name}
+        ], function(err, result) {
+          assert.equal(err, null);
+        });
+        ctx.body = {success:true};
+    }else{
+        ctx.body = {success:false};
+    }
 });
 
 router.post('/add', async (ctx,next)=>{
-    console.log("+++++++",ctx.request.body);
-    ctx.body="hello world";
+    var {content,name} = ctx.request.body;
+    if(content && name){
+        var collection = ctx.mongo.collection('messages');
+        // Insert some documents
+        collection.insertMany([
+          {content:content,name:name}
+        ], function(err, result) {
+          assert.equal(err, null);
+        });
+        ctx.body = {success:true};
+    }else{
+        ctx.body = {success:false};
+    }
 });
 
 router.use('/message', router.routes(), router.allowedMethods());
